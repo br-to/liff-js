154a31581cecba7ef2bf0ba3f22b58a619c8590c
br-to <barabara0224@gmail.com>
2021-04-05 23:20:59 +0900

    liffブラウザ制御

--- bundle.js
+++ bundle.js
@@ -4,7 +4,7 @@
 window.onload = function () {
   liff.init({ liffId: "1655727472-xvE2Zm18" },
     data => {
-      if (liff.isLoggedIn()) {
+      if (liff.isLoggedIn() && liff.isInClient()) {
         alert(liff.getAccessToken());
       } else {
         liff.login();
