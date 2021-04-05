c9096587db382a611e4c0f3cff249ded8b97a716
br-to <barabara0224@gmail.com>
2021-04-05 23:03:57 +0900

    token

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 
 
 window.onload = function () {
-  liff.init(
-    { liffId: "1655727472-xvE2Zm18" },
-    (data) => {
-      if (liff.isLoggedIn()) {
+  liff.init({ liffId: "1655727472-xvE2Zm18" },
+    data => {
+      if (liff.isLoggedIn() && liff.isInClient()) {
         alert(liff.accessToken());
       } else {
         liff.login();
       }
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
-};
+      },
+      (error) => {
+        console.log(error);
+    });
+}
 
 
 
