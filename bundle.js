93b1cbeab6ff315613ff8ada61a299a716473629
br-to <barabara0224@gmail.com>
2021-04-05 22:56:47 +0900

    liffログイン検証

--- bundle.js
+++ bundle.js
@@ -5,7 +5,7 @@ window.onload = function () {
   liff.init({ liffId: "1655727472-xvE2Zm18" },
     data => {
       if (liff.isLoggedIn()) {
-        alert("ios!");
+        alert(liff.accessToken());
       } else {
         liff.login();
       }
